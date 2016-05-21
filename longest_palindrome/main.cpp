61c88ea4d2ee8957be4d3d9724a1d53e47c2ce53
Gleb Dolgich <glebd70@gmail.com>
2016-05-22 00:53:52 +0100

    Initial commit: add longest_palindrome project

---
+++ longest_palindrome/main.cpp
@@ -0,0 +1,20 @@
+// http://stevekrenzel.com/articles/longest-palnidrome
+// Find the longest palindrome in a string
+
+#include <string>
+
+#include "catch.hpp"
+
+using namespace std;
+
+TEST_CASE("racecar") {
+
+}
+
+TEST_CASE("racecars") {
+
+}
+
+TEST_CASE("kayak racecars") {
+
+}
