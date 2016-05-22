ecee556852a6c1faa32fe7a23b50f682963c793e
Gleb Dolgich <glebd70@gmail.com>
2016-05-22 02:55:34 +0100

    [longest_palindrome] Implement longest_palindrome_at_index()

--- longest_palindrome/main.cpp
+++ longest_palindrome/main.cpp
@@ -7,6 +7,26 @@
 
 using namespace std;
 
+// Calculates length of the palindrome centered at the passed string range.
+// The length will be at least 1 as a single letter is a palindrome.
+tuple<string, size_t> longest_palindrome_at_index(const string& s, size_t center_index) {
+  if (s.empty()) return {s, 0};
+  if (s.length() == 1) return {s, 1};
+  long left = center_index;
+  long right = center_index;
+  size_t length = 0;
+  // Check for even-length palindromes that have 2 chars in the middle
+  if (center_index < s.length() - 1 && s[center_index + 1] == s[center_index]) {
+    ++right;
+  }
+  // Expand palindrome range until we either hit the string bounds or the result is not a palindrome
+  while (left >= 0 && right < s.length() && s[left] == s[right]) {
+    --left;
+    ++right;
+  }
+  return {s.substr(static_cast<size_t>(left), static_cast<size_t>(right - left)), right - left};
+}
+
 string longest_palindrome(const string& s) {
   return "";
 }
