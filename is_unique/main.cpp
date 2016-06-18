c6b2b0d793337c2bcd9855c298d554cf4eccd3a1
Gleb Dolgich <glebd70@gmail.com>
2016-06-18 22:58:20 +0100

    Add skeleton project

---
+++ is_unique/main.cpp
@@ -0,0 +1,14 @@
+/**
+ * Implement an algorithm to determine if a string has all unique characters.
+ * What if you cannot use additional data structures?
+ *
+ * Question 1.1, p. 90, Cracking the Coding Interview 6th ed. by Gayle Laakmann McDowell
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello, World!" << endl;
+  return 0;
+}
