3513e1092eabc0ced0e0a9871bb94c8234bc564c
Gleb Dolgich <glebd70@gmail.com>
2016-06-19 03:51:17 +0100

    Set up gtest, add dummy test case

--- is_unique/main.cpp
+++ is_unique/main.cpp
@@ -6,9 +6,11 @@
  */
 
 #include <iostream>
+
+#include <gtest/gtest.h>
+
 using namespace std;
 
-int main() {
-  cout << "Hello, World!" << endl;
-  return 0;
+TEST(basic_check, test_all_unique) {
+  EXPECT_EQ(1, 1);
 }
