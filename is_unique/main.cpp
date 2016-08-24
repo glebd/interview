5a7b5792cbbb89ad6cc80770e777b10f183e8ae7
Gleb Dolgich <glebd@me.com>
2016-08-24 02:53:23 +0100

    Add minimal tests

--- is_unique/main.cpp
+++ is_unique/main.cpp
@@ -11,6 +11,23 @@
 
 using namespace std;
 
-TEST(basic_check, test_all_unique) {
-  EXPECT_EQ(1, 1);
+bool is_all_unique_chars(const string& s) {
+  if (s.empty()) return true;
+  return false;
+}
+
+TEST(basic_check, all_unique) {
+  EXPECT_EQ(is_all_unique_chars("abcd"), true);
+}
+
+TEST(basic_check, some_unique) {
+  EXPECT_EQ(is_all_unique_chars("abcda"), false);
+}
+
+TEST(basic_check, none_unique) {
+  EXPECT_EQ(is_all_unique_chars("aaaaa"), false);
+}
+
+TEST(edge_check, empty_string) {
+  EXPECT_EQ(is_all_unique_chars(""), true);
 }
