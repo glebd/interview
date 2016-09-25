755f34848280985dc5d84cfd6bd721f284e689f7
Gleb Dolgich <glebd70@gmail.com>
2016-09-25 01:58:25 +0100

    Add boilerplate for mvn02 assignment

---
+++ mvn02/main.cpp
@@ -0,0 +1,17 @@
+//
+// Given an array of integers that represent base 2 exponents,
+// the binarian is the sum of 2 raised to power of each element.
+// Determine the minimum number of array elements required
+// to represent the binarian value of the input array.
+//
+
+#include <vector>
+
+#include "catch.hpp"
+
+using namespace std;
+
+int binarian(const vector<int> input)
+{
+    return 0;
+}
